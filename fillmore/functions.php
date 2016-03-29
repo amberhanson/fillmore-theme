f454e9c662ecce9bb29a9442873836dba5f1d466
amberhanson <hanson.amber.m@gmail.com>
2016-03-28 20:52:26 -0500

    Added files via upload

--- fillmore/functions.php
+++ fillmore/functions.php
@@ -6,7 +6,6 @@
  *
  * @package fillmore
  */
-
 if ( ! function_exists( 'fillmore_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -23,10 +22,8 @@ function fillmore_setup() {
 	 * to change 'fillmore' to the name of your theme in all the template files.
 	 */
 	load_theme_textdomain( 'fillmore', get_template_directory() . '/languages' );
-
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
-
 	/*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
@@ -34,19 +31,16 @@ function fillmore_setup() {
 	 * provide it for us.
 	 */
 	add_theme_support( 'title-tag' );
-
 	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
-
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'fillmore' ),
 	) );
-
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
@@ -58,7 +52,6 @@ function fillmore_setup() {
 		'gallery',
 		'caption',
 	) );
-
 	/*
 	 * Enable support for Post Formats.
 	 * See https://developer.wordpress.org/themes/functionality/post-formats/
@@ -70,7 +63,6 @@ function fillmore_setup() {
 		'quote',
 		'link',
 	) );
-
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'fillmore_custom_background_args', array(
 		'default-color' => 'ffffff',
@@ -79,7 +71,6 @@ function fillmore_setup() {
 }
 endif;
 add_action( 'after_setup_theme', 'fillmore_setup' );
-
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -91,7 +82,6 @@ function fillmore_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'fillmore_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'fillmore_content_width', 0 );
-
 /**
  * Register widget area.
  *
@@ -109,46 +99,43 @@ function fillmore_widgets_init() {
 	) );
 }
 add_action( 'widgets_init', 'fillmore_widgets_init' );
-
 /**
  * Enqueue scripts and styles.
  */
 function fillmore_scripts() {
 	wp_enqueue_style( 'fillmore-style', get_stylesheet_uri() );
-        
-        //Add Google Fonts
+	
+	//Add Google Fonts
         wp_enqueue_style('fillmore-google-fonts', 'https://fonts.googleapis.com/css?family=Dosis:400,500,700,300,200|Merriweather:400,700,700italic,400italic|Open+Sans:400,700,400italic,700italic');
 
-	wp_enqueue_script( 'fillmore-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'fillmore-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+	wp_enqueue_script( 'fillmore-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20120206', true );
+	wp_localize_script( 'fillmore-navigation', 'screenReaderText', array(
+		'expand'   => '<span class="screen-reader-text">' . __( 'expand child menu', 'fillmore' ) . '</span>',
+		'collapse' => '<span class="screen-reader-text">' . __( 'collapse child menu', 'fillmore' ) . '</span>',
+	) );
+	
+	wp_enqueue_script( 'fillmore-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'fillmore_scripts' );
-
 /**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
-
 /**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
 /**
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/inc/extras.php';
-
 /**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
 /**
  * Load Jetpack compatibility file.
  */
