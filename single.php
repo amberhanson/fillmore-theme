3ad7ffd25f0f0fc4fb004467a2bd4dda97f7430c
amberhanson <hanson.amber.m@gmail.com>
2016-04-13 16:50:48 -0500

    Added files via upload

--- single.php
+++ single.php
@@ -12,23 +12,31 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			get_template_part( 'template-parts/content', get_post_format() );
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			the_post_navigation();
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+			
+			<?php 
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'fillmore' ) . '</span> ',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Prev Post', 'fillmore' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'fillmore' ) . '</span> ',
+			) );
+			?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+		<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
 
 <?php
 get_sidebar();
